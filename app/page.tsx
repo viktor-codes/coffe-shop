2d6adef5a41332c43db01d558eb105b7fa118240
Viktor Rura <ruravictor@gmail.com>
2025-11-04 20:27:40 +0000

    big project structure update

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import Hero from '@/sections/hero'
+import Hero from '@/components/sections/hero/hero'
 import Navbar from '@/components/layout/navbar/navbar'
-import Footer from '@/components/footer'
-import Features from '@/sections/features'
-import Menu from '@/sections/menu'
-import About from '@/sections/about'
-import Testimonials from '@/sections/testimonials'
-import Reservation from '@/sections/reservation'
-import Articles from '@/sections/articles'
+import Footer from '@/components/layout/footer/footer'
+import Features from '@/components/sections/features/features'
+import Menu from '@/components/sections/menu/menu'
+import About from '@/components/sections/about/about'
+import Testimonials from '@/components/sections/testimonials/testimonials'
+import Reservation from '@/components/sections/reservation/reservation'
+import Articles from '@/components/sections/articles/articles'
 
 export default function Home() {
 	return (
